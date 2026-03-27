1836f5c2b5506d6593cb446adaee803eba31b040
codewithroll <vianshu12@gmail.com>
2026-03-28 02:17:47 +0530

    Updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const contactRoutes = require("./routes/contactRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
