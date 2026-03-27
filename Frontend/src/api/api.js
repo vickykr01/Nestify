bfb48572a41377925b9b6634b7750dc9d8c4f36e
codewithroll <vianshu12@gmail.com>
2026-03-28 02:01:54 +0530

    Update api.js

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://nestify-27oo.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
